c857291ce483d29ae6ea75d198c09dd2dcb05110
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-22 17:47:01 -0400

    app.py implementation

--- app.py
+++ app.py
@@ -1,2 +1,36 @@
 from flask import Flask, request, jsontify, render_template, redirect, url_for, session
 
+app = Flask(__name__) # Initialize the Flask application
+app.secret = "your_secret_key"  # Set a secret key for session management   
+
+render_template("login.html")  # Render the login page
+
+#dictionary will serve as our database
+users = {}
+
+
+# Dealing with a GET request
+@app.route("handle_request", methods = ["GET"])
+def handle_get():
+    if request.method == "GET":
+        username = request.args("username")
+        password = request.args("password")
+        if username in users and users[username] == password:
+            return jsontify("Welcome!! You were successfully logged in."),200
+        else:
+            return jsontify("Invalid Credentials"),401
+    else:
+        return render_template("login.html")
+    
+#Dealing with a POST request
+@app.route("handle_request", methods = ["POST"])
+def handle_post():
+    if request.method == "POST":
+        username = request.form("username")
+        password = request.form("password")
+        if username in users and users[username] == password:
+            return jsontify("Welcome!! You were successfully logged in."), 200
+        else:
+            return jsontify("Invalid Credentials"), 401
+    else:
+        return render_template("login.html")
