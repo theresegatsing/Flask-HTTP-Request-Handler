f6551370cc37b951746c5b477114ca12982c4435
Therese Elvira Mombou Gatsing <tmombougatsing@mail.wlu.edu>
2025-07-22 18:28:31 -0400

    updated app.py code

--- app.py
+++ app.py
@@ -1,36 +1,45 @@
-from flask import Flask, request, jsontify, render_template, redirect, url_for, session
+from flask import Flask, request, render_template,  session
 
 app = Flask(__name__) # Initialize the Flask application
-app.secret = "your_secret_key"  # Set a secret key for session management   
+app.secret_key = "my_secret_key" #Set a secret key for session management   
 
-render_template("login.html")  # Render the login page
+
 
 #dictionary will serve as our database
-users = {}
+users = {"yaniv" : "0225", "elvi" : "1247"}
+
+# Render the login page
+@app.route('/')
+def view_form():
+    return render_template("login.html")
 
 
 # Dealing with a GET request
-@app.route("handle_request", methods = ["GET"])
+@app.route('/handle_get', methods=["GET"])
 def handle_get():
     if request.method == "GET":
-        username = request.args("username")
-        password = request.args("password")
+        username = request.args["username"]
+        password = request.args["password"]
         if username in users and users[username] == password:
-            return jsontify("Welcome!! You were successfully logged in."),200
+            return'<h1>Welcome!!!</h1>'
         else:
-            return jsontify("Invalid Credentials"),401
+            return '<h1>invalid credentials!</h1>'
     else:
         return render_template("login.html")
     
 #Dealing with a POST request
-@app.route("handle_request", methods = ["POST"])
+@app.route('/handle_post', methods=["POST"])
 def handle_post():
     if request.method == "POST":
-        username = request.form("username")
-        password = request.form("password")
+        username = request.form["username"]
+        password = request.form["password"]
         if username in users and users[username] == password:
-            return jsontify("Welcome!! You were successfully logged in."), 200
+            return '<h1>User Already exists!" </h1>'
         else:
-            return jsontify("Invalid Credentials"), 401
+            users[username] = password
+            return 'h1> User Successfully registered!</h1>'
     else:
         return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)  # Run the Flask application in debug mode
